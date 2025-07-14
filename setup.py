6dc6183c7aa4d1548bcfaabb56a9754b951d800f
lucius krieguer <fuentesnmejiaj@gmail.com>
2025-07-14 14:35:48 -0500

    mi primer lanzamiento  pip

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="quintorepo",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Nohelia Catalina Fuentes Cruz",                         # Tu nombre
+    author_email="nfuentes@celerix.com",                 # Tu correo electrónico
+    url="https://github.com/noheliaFuentescelerix/Quintorepo",     # URL del proyecto
+)
